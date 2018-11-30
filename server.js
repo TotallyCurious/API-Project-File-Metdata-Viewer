33eaab8224c264240fbae58a4f20fa1ffdb66446
Glitch (spring-base) <none>
2018-11-30 07:27:02 +0000

    💶🏘 Checkpoint ./package.json:2018030/23 ./server.js:2018030/186

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -14,8 +16,8 @@ app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.post('/api/fileanalyse', upload.single('image'),function(req, res){
+  res.json({details: req.body},(e,d)=>{});
 });
 
 app.listen(process.env.PORT || 3000, function () {
