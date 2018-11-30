2d44343fe8f31d63da3f5a4eadc6e71039e9688f
Glitch (spring-base) <none>
2018-11-30 07:38:02 +0000

    🎍👢 Checkpoint ./server.js:2018030/128

--- server.js
+++ server.js
@@ -12,12 +12,15 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+//easy print function
+var p = (val)=>{console.log(val)};
+
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
 app.post('/api/fileanalyse', upload.single('image'),function(req, res){
-  res.json({details: req.body},(e,d)=>{});
+  res.json({details: req.file},(e,d)=>{e?p(e):p(d);});
 });
 
 app.listen(process.env.PORT || 3000, function () {
